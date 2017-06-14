4e0b12e738166f962e60878722b11307f00c4bad
mrmnty <amit007mohanty@gmail.com>
2017-06-14 18:13:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect To Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
+});
+
+const app = express();
+
+const users = require("./routes/users");
+
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use("/users", users);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, ()=>{
+    console.log("server started on port" + port);
+});
